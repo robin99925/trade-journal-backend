d93fbf9c35a2533936b55cdf2928d4d58dfd87c5
Robin Saini <sainirobin476@gmail.com>
2026-06-21 15:28:54 +0530

    vercel setup

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const routes = require("./routes");
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
+
+// Health Check Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running successfully",
+  });
+});
 
 app.use("/api/v1", routes);
 
